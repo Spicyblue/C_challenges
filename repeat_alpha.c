308a5df4b0b95144f4ef58e7215830f04aec9ade
Spicyblue <ochulor.okechukwu@gmail.com>
2025-09-11 17:33:37 +0200

    Non-Norm Verified Rank 02 Level 1

---
+++ repeat_alpha.c
@@ -0,0 +1,90 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   repeat_alpha.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: okochulo <okochulo@student.42vienna.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/11 16:45:01 by okochulo          #+#    #+#             */
+/*   Updated: 2025/09/11 17:31:30 by okochulo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// Assignment name  : repeat_alpha
+// Expected files   : repeat_alpha.c
+// Allowed functions: write
+// ------------------------------------------------------------------------------
+
+// Write a program called repeat_alpha that takes a string and display it
+// repeating each alphabetical character as many times as its alphabetical index,
+// followed by a newline.
+// 'a' becomes 'a', 'b' becomes 'bb', 'e' becomes 'eeeee', etc...
+// Case remains unchanged.
+// If the number of arguments is not 1, just display a newline.
+// Examples:
+// $>./repeat_alpha "abc"
+// abbccc
+// $>./repeat_alpha "Alex." | cat -e
+// Alllllllllllleeeeexxxxxxxxxxxxxxxxxxxxxxxx.$
+// $>./repeat_alpha 'abacadaba 42!' | cat -e
+// abbacccaddddabba 42!$
+// $>./repeat_alpha | cat -e
+// $
+// $>
+// $>./repeat_alpha "" | cat -e
+// $
+// $>
+
+#include <unistd.h>
+
+int	is_alpha(int c)
+{
+	return ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'));
+}
+
+int is_lower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
+
+int	is_upper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
+int	main(int argc, char **argv)
+{
+	size_t	ct;
+	size_t	i;
+
+	ct = 0;
+	i = 0;
+	if (argc == 2)
+	{
+		while (argv[1][i] != '\0')
+		{
+			if (is_alpha(argv[1][i]) && is_upper(argv[1][i]))
+				{
+					ct = argv[1][i] - '@';
+					while (ct)
+						{
+							write(1, &argv[1][i], 1);
+							ct--;
+						}
+				}
+			else if (is_alpha(argv[1][i]) && is_lower(argv[1][i]))
+				{
+					ct = argv[1][i] - '`';
+					while (ct)
+					{
+						write(1, &argv[1][i], 1);
+						ct--;
+					}
+                }
+            else 
+                write (1, &argv[1][i], 1);
+            i++;
+		}
+	}
+	write(1, "\n", 1);
+	return (0);
+}
